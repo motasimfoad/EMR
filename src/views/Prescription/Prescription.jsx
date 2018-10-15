22ac169389391ce38323957e2808fb17708d9b21
motasimfoad <motasimfoad@gmail.com>
2018-10-16 01:14:42 +0600

    Prescription modal

--- src/views/Prescription/Prescription.jsx
+++ src/views/Prescription/Prescription.jsx
@@ -9,18 +9,56 @@ import {
   Col,
   Navbar,
   Form, 
+  Modal, 
+  ModalHeader, 
+  ModalBody, 
+  ModalFooter
 } from "reactstrap";
 import Button from "components/CustomButton/CustomButton.jsx";
 
 
 class Prescription extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false,
+      nestedModal: false,
+      closeAll: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+    this.toggleNested = this.toggleNested.bind(this);
+    this.toggleAll = this.toggleAll.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  toggleNested() {
+    this.setState({
+      nestedModal: !this.state.nestedModal,
+      closeAll: false
+    });
+  }
+
+  toggleAll() {
+    this.setState({
+      nestedModal: !this.state.nestedModal,
+      closeAll: true
+    });
+  }
+
   render() {
     return (
       <div className="content">
       
       <Navbar expand="lg" color="dark">
       <Form inline className="ml-auto">
-      <Button color="primary" round outline>
+      <Button onClick={this.toggle} color="primary" round outline>
       <i className="fa fa-plus"></i> Create
       </Button>
         
@@ -152,6 +190,27 @@ class Prescription extends React.Component {
       
       
       </Row>
+      
+      <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.toggle}>Create Prescription</ModalHeader>
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <br />
+            <Button color="success" onClick={this.toggleNested}>Show Nested Modal</Button>
+            <Modal isOpen={this.state.nestedModal} toggle={this.toggleNested} onClosed={this.state.closeAll ? this.toggle : undefined}>
+              <ModalHeader>Nested Modal title</ModalHeader>
+              <ModalBody>Stuff and things</ModalBody>
+              <ModalFooter>
+                <Button color="primary" onClick={this.toggleAll}>Done</Button>{' '}
+                <Button color="secondary" onClick={this.toggleAll}>All Done</Button>
+              </ModalFooter>
+            </Modal>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="success" onClick={this.toggleNested}>Create</Button>{' '}
+            <Button color="danger" onClick={this.toggleNested}>Discard</Button>
+          </ModalFooter>
+        </Modal>
 
       </div>
     );
