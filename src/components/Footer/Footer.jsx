f37dbfbf3192d7b784b5e66fb992827c2c80ec89
motasimfoad <motasimfoad@gmail.com>
2018-10-15 22:54:01 +0600

    Cleaned header for every pages

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,22 +12,9 @@ class Footer extends React.Component {
       >
         <Container fluid={this.props.fluid ? true : false}>
           <Row>
-            <nav className="footer-nav">
-              <ul>
-                <li>
-                  <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>
-                </li>
-                <li>
-                  <a href="https://blog.creative-tim.com" target="_blank">Blog</a>
-                </li>
-                <li>
-                  <a href="https://www.creative-tim.com/license" target="_blank">Licenses</a>
-                </li>
-              </ul>
-            </nav>
             <div className="credits ml-auto">
               <div className="copyright">
-                &copy; {1900 + new Date().getYear()}, made with <i className="fa fa-heart heart"></i> by Creative Tim
+                &copy; {1900 + new Date().getYear()}, made with <i className="fa fa-heart heart"></i> by EMR
               </div>
             </div>
           </Row>
--- src/components/Header_prescripton/Header.jsx
+++ src/components/Header_prescripton/Header.jsx
@@ -135,7 +135,7 @@ class Header extends React.Component {
                 <span className="navbar-toggler-bar bar3" />
               </button>
             </div>
-            <NavbarBrand>{this.getBrand()}</NavbarBrand>
+            <NavbarBrand href="/">{this.getBrand()}</NavbarBrand>
           </div>
           <NavbarToggler onClick={this.toggle}>
             <span className="navbar-toggler-bar navbar-kebab" />
@@ -147,6 +147,51 @@ class Header extends React.Component {
             navbar
             className="justify-content-end"
           >
+            <form>
+              <InputGroup className="no-border">
+                <Input placeholder="Search..." />
+                <InputGroupAddon addonType="append">
+                  <InputGroupText>
+                    <i className="nc-icon nc-zoom-split" />
+                  </InputGroupText>
+                </InputGroupAddon>
+              </InputGroup>
+            </form>
+            <Nav navbar>
+              <NavItem>
+                <Link to="#pablo" className="nav-link btn-magnify">
+                  <i className="nc-icon nc-layout-11" />
+                  <p>
+                    <span className="d-lg-none d-md-block">Stats</span>
+                  </p>
+                </Link>
+              </NavItem>
+              <Dropdown
+                nav
+                isOpen={this.state.dropdownOpen}
+                toggle={e => this.dropdownToggle(e)}
+              >
+                <DropdownToggle caret nav>
+                  <i className="nc-icon nc-bell-55" />
+                  <p>
+                    <span className="d-lg-none d-md-block">Some Actions</span>
+                  </p>
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem tag="a">Action</DropdownItem>
+                  <DropdownItem tag="a">Another Action</DropdownItem>
+                  <DropdownItem tag="a">Something else here</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+              <NavItem>
+                <Link to="#pablo" className="nav-link btn-rotate">
+                  <i className="nc-icon nc-settings-gear-65" />
+                  <p>
+                    <span className="d-lg-none d-md-block">Account</span>
+                  </p>
+                </Link>
+              </NavItem>
+            </Nav>
           </Collapse>
         </Container>
       </Navbar>
