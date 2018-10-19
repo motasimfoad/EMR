eb6582b313777949f4a0f3f5eddd3a9dc015d9cf
motasimfoad <motasimfoad@gmail.com>
2018-10-19 17:39:45 +0600

    Updates

---
+++ precache-manifest.6db8ebf6ab2f75e0e841fa043857f6d8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/EMR/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "7a4e732fffd6fa6eb33a",
+    "url": "/EMR/static/css/main.3f57b27e.chunk.css"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/EMR/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "c36d06a66ac367beff29",
+    "url": "/EMR/static/js/1.c36d06a6.chunk.js"
+  },
+  {
+    "revision": "d02e2d9c8d0de31bb889",
+    "url": "/EMR/static/js/runtime~main.d02e2d9c.js"
+  },
+  {
+    "revision": "a6f24e7dd5470a8bd2f48ebb53dbe8d5",
+    "url": "/EMR/static/media/damir-bosnjak.a6f24e7d.jpg"
+  },
+  {
+    "revision": "aab414f7b69d0d4ad897e75e735f240a",
+    "url": "/EMR/static/media/mike.aab414f7.jpg"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/EMR/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "7a4e732fffd6fa6eb33a",
+    "url": "/EMR/static/js/main.7a4e732f.chunk.js"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/EMR/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/EMR/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/EMR/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/EMR/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/EMR/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "c36d06a66ac367beff29",
+    "url": "/EMR/static/css/1.9f24cdf0.chunk.css"
+  },
+  {
+    "revision": "2b9ad3fdca7bd24e3287761ddc85ec67",
+    "url": "/EMR/index.html"
+  }
+];
