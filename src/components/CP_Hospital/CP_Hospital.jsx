684aa4e976cb6a625befc7bb4f18278b7ed22774
motasimfoad <motasimfoad@gmail.com>
2018-11-19 04:09:55 +0600

    Worked with the styling and screens of create report page for hospital

--- src/components/CP_Hospital/CP_Hospital.jsx
+++ src/components/CP_Hospital/CP_Hospital.jsx
@@ -9,7 +9,7 @@ import {
   Row,
   Col,
   FormGroup,
-  Label,
+  Label, Modal, ModalHeader, ModalBody, ModalFooter ,
   Input
  } from "reactstrap";
 import Button from "components/CustomButton/CustomButton.jsx";
@@ -17,25 +17,19 @@ import gql from "graphql-tag";
 import { Mutation } from "react-apollo";
 import ReactLoading from 'react-loading';
 import Footer from "../Footer/Footer.jsx";
-//import { graphql } from 'react-apollo';
 import Dropzone from 'react-dropzone';
 import {client} from "../../index";
+import Loader from 'react-loader-spinner'
 
 class CP_Hospital extends React.Component {
 
   constructor(props) {
     super(props);
-//     this.state = {
-//       pname: '',
-//       nid: '',
-//       inputAddress: '',
-//       phoneid: '',
-//       inputDetails: '',
-//       inputMed: ''
-// };
-      //this.handleChange = this.handleChange.bind(this);
+
       this.back = this.back.bind(this);
       this.submit = this.submit.bind(this);
+      this.lo = this.lo.bind(this);
+      this.toggle = this.toggle.bind(this);
 
       if (typeof(props.history.location.state) == 'undefined' || props.history.location.state == null) {
         this.props.history.push({
@@ -44,6 +38,7 @@ class CP_Hospital extends React.Component {
       }
       else {
           this.state = {
+            modal: false,
             phoneid: '',
             hospitalId : this.props.history.location.state.logInfo[0],
             logInfoToken : this.props.history.location.state.logInfo[1],
@@ -55,49 +50,83 @@ class CP_Hospital extends React.Component {
 
   }
 
+  lo() {
+    this.props.history.push({
+      pathname: '/auth'
+     
+    });
+  }
+
   back() {
     this.props.history.push({
       pathname: '/hospitaldashboard',
-      state: { logInfo: [this.state.drid, 
+      state: { logInfo: [this.state.hospitalId, 
         this.state.logInfoToken,
-        this.state.docname
+        this.state.hospitalName,
+        this.state.phnno,
       ] }
     });
   }
 
-  // handleChange(evt) {
-  //   this.setState({ [evt.target.id]: evt.target.value });
-  // }
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
 
   render() {
 
     return (
 
-   <div className="content">
-     <div className='w-100 pa4 flex justify-center'>
-        <div style={{ maxWidth: 400 }} className=''>
-          <input
-            className='w-100 pa3 mv2'
+   <div className="cphelperMain">
+        <h2>Add prescription for a Patient</h2>
+        <pre className="cpHospitalText">Enter the patients NID/Birth Cirtificate no/ Passport No/ Phone No. <br/>
+        Upload the image of your prescription.<br/>
+        The submit button will automatically apper on your screen.
+        </pre>
+        <div style={{ maxWidth: 400 }} className='cphelperAfterMain'>
+          <Input
+            className='cpHospitalInput'
             value={this.state.phoneid}
-            placeholder='Description'
+            placeholder='NID/Birth Cirtificate no/ Passport No/ Phone No'
             onChange={(e) => this.setState({phoneid: e.target.value})}
           />
+          <br/>
           {!this.state.imageId &&
           <Dropzone
+            className="dz"
             onDrop={this.onDrop}
             accept='image/*'
             multiple={false}
           >
-            <div>Drop an image or click to choose</div> {this.state.imageId} {this.state.imageUrl}
+            <div><b>Drop an image or click to choose</b></div> 
           </Dropzone>}
           {this.state.imageUrl &&
             <img src={this.state.imageUrl} role='presentation' className='w-100 mv3' />
           }
           {this.state.phnno && this.state.imageUrl &&
-            <button className='pa3 bg-black-10 bn dim ttu pointer' onClick={this.submit}>Post</button>
+            <Button color="success" className="searchBtHelper" onClick={this.submit}><i className="fa fa-hdd "/> &nbsp;Post</Button>
           }
+          <Button type="submit" value="Submit" color="info" className="searchBtHelper" onClick={this.back}><i className="fa fa-angle-double-left "/> &nbsp;Back</Button>
+          <Button type="submit" value="Submit" color="danger" className="searchBtHelper" onClick={this.lo}><i className="fa fa-sign-out-alt "/> &nbsp;Log Out</Button>
+        
+          
+    <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+      <ModalHeader toggle={this.toggle}></ModalHeader>
+      <ModalBody className="crhosCenter">
+        <img src ="https://sparkflow.co/wp-content/uploads/icon-form-check.png" />
+        <br />
+       <h3>Report Successfully Added</h3>
+      </ModalBody>
+      <ModalFooter>
+        <Button color="info" onClick={this.back}>Back</Button>
+        <Button color="danger" onClick={this.lo}>Logout</Button>
+      </ModalFooter>
+    </Modal>
+  
+        
         </div>
-      </div>
+      
 
       
 < Footer />
@@ -106,6 +135,7 @@ class CP_Hospital extends React.Component {
     
   }
   onDrop = (files) => {
+    
     // prepare form data, use data key!
     let data = new FormData()
     data.append('data', files[0])
@@ -124,35 +154,6 @@ class CP_Hospital extends React.Component {
     })
   }
 
-//   handlePost = async () => {
-//     await client.mutate({
-//       mutation: gql`
-//          mutation createReport($imageID: String, $imageUrl: String,  $phoneid: String){
-//           createReport(
-//            imageid : $imageID ,
-//            imageUrl : $imageUrl ,
-//            phoneid : $phoneid
-//         ) {
-//           id
-//           phoneid
-//         }
-//       }
-//       `,
-      
-//       variables: {
-//         imageID: this.state.nid,
-//         imageUrl: this.state.imageUrl,
-//         phoneid: this.state.phoneid
-//       },
-  
-//     })
-//     .then(result => { 
-//       alert(result.data.createReport.id)
-//       console.log(result.data.createReport.phoneid);
-      
-//      })
-//     .catch(error => { console.log(error)});
-// }
 
 async submit(){
   await client.mutate({
@@ -177,31 +178,22 @@ async submit(){
     },
 
   })
-  .then(result => { 
-    alert(result.data.createReport.id)
-    console.log(result.data.createReport.phoneid);
-    console.log(result.data.createReport.imageUrl);
+  .then(result => {
+    this.toggle();
+ // alert(result.data.createReport.id)
+    // console.log(result.data.createReport.phoneid);
+    // console.log(result.data.createReport.imageUrl);
+  } 
+    
+
+
+   
     
     
-   })
+   )
   .catch(error => { console.log(error)});
 };
 
 }
-
-// const addMutation = gql`
-//   mutation addPost($imageid: String!, $imageUrl: ID!) {
-//     createPost(imageid: $imageid, imageUrl: $imageUrl) {
-//       id
-//       imageUrl
-//       imageid
-//       }
-//     }
-//   }
-// `
-
-//const PageWithMutation = graphql(addMutation, { name: 'addPost' })(CP_Hospital)
-
-// export default withRouter(PageWithMutation)
 
 export default CP_Hospital;
